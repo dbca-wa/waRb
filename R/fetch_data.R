3e86ed83943511758638047074f9e8429623c293
Florian Mayer <Florian.Mayer@dbca.wa.gov.au>
2022-07-15 13:10:02 +0800

    Init

---
+++ R/fetch_data.R
@@ -0,0 +1,11 @@
+#' Fetch data from the Data Catalogue and return as one object
+#'
+#' @value None
+#' @import ckanr
+#' @export
+#' @family extract
+fetch_data <- function(){
+  message('Fetch data')
+}
+
+# usethis::use_test("fetch_data")
---
+++ tests/testthat/test-fetch_data.R
@@ -0,0 +1,5 @@
+test_that("fetch_data works", {
+  expect_message(fetch_data())
+})
+
+# usethis::use_r("fetch_data")
